39391221f62d5ce6b8371da322e8c77873e84a58
Zenobius Jiricek <airtonix@users.noreply.github.com>
2021-07-30 09:30:13 +0930

    lint ignore rollup

--- packages/scheduled-transactions/rollup.config.js
+++ packages/scheduled-transactions/rollup.config.js
@@ -1,5 +1,6 @@
 import { createConfig } from "@budget-functions/create-rollup-config";
 
+// eslint-disable-next-line import/no-default-export
 export default createConfig({
   input: "src/index.ts",
   outputFile: "dist/scheduled-transactions.js",
